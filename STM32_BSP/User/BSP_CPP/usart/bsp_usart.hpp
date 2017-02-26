e76d8598bb1c03004fd0f1f3138de9ab42180d88
Chen_Li@HomePC <luke.linus.chen@gmail.com>
2017-02-26 19:08:10 +0800

    usart 小幅调整

--- STM32_BSP/User/BSP_CPP/usart/bsp_usart.hpp
+++ STM32_BSP/User/BSP_CPP/usart/bsp_usart.hpp
@@ -3,7 +3,6 @@
 
 #include "stm32f4xx.h"
 #include "util.h"
-//#include <stdio.h>
 
 class Usart
 {
@@ -17,7 +16,9 @@ public:
     void SendStr(const byte * str);
 
 protected:
-    void NvicConfig(uint8_t prePriority, uint8_t subPriority);
+    void NvicConfig(uint8_t prePriority, uint8_t subPriority);  // 设置中断优先级
+    virtual void rccGPIOCmd(uint32_t, FunctionalState)  = 0;    // 管脚IO时钟控制函数
+    virtual void rccUsartCmd(uint32_t, FunctionalState) = 0;    // 串口时钟控制函数
 
     USART_TypeDef* m_usart; // 串口名
 
@@ -26,28 +27,17 @@ protected:
     uint32_t m_usartClk;    // 串口时钟
     IRQn m_irq;             // 中断号
 
-    virtual void rccGPIOCmd(uint32_t, FunctionalState) = 0;     // 管脚IO时钟控制函数
-    virtual void rccUsartCmd(uint32_t, FunctionalState) = 0;    // 串口时钟控制函数
-
     GPIO_TypeDef* m_txPort; // 发送管脚PORT
     GPIO_TypeDef* m_rxPort; // 接收管脚PORT
 
-    uint16_t m_txPin;   // 发送管脚PIN
-    uint16_t m_rxPin;   // 接收管脚PIN
+    uint16_t m_txPin;       // 发送管脚PIN
+    uint16_t m_rxPin;       // 接收管脚PIN
 
-    uint8_t m_txSource; // 发送管脚SOURCE
-    uint8_t m_rxSource; // 接收管脚SOURCE
+    uint8_t m_txSource;     // 发送管脚SOURCE
+    uint8_t m_rxSource;     // 接收管脚SOURCE
 
-    uint8_t m_gpioAF;   // GPIO管脚复用目标
-
+    uint8_t m_gpioAF;       // GPIO管脚复用目标
 };
 
 
-/*#define DEBUG_USART                             USART1*/
-//int fputc(int ch, FILE *f);
-//int fgetc(FILE *f);
-
 #endif  // __BSP_USART_HPP
-
-
-
